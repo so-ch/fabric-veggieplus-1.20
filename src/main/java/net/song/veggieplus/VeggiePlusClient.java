dff07e2b76c332ab20b8c20d3db88aa1d10dd9a1
Song Chun <song@Songs-Air-M1.local>
2023-07-30 15:13:45 +0900

    Initial commit

---
+++ src/main/java/net/song/veggieplus/VeggiePlusClient.java
@@ -0,0 +1,10 @@
+package net.song.veggieplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VeggiePlusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
